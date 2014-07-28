80e244f9d52f061b20dbacd42c8d8bb3e7e1fb0f
Brother Rain <brother.rain.1024@gmail.com>
2014-07-28 07:05:00 +0000

    update g++

--- cpp/config.js
+++ cpp/config.js
@@ -1,7 +1,7 @@
 Config = {
-   "name" : "CPP Documentation",
-   "introduction": "<b>CPP</b> is a <tt>distributed revision control</tt> and <tt>source code management (SCM) system</tt> with an <b>emphasis</b> on <b>speed</b>, <b>data integrity</b>, and support for <b>distributed, non-linear workflows</b>. Git was initially designed and developed by <b>Linus Torvalds</b> for Linux kernel development in <b>2005</b>, and has since become the most widely adopted version control system for software development",
-   "version": "1.0"
+   "name" : "C++",
+   "introduction": "C++ (pronounced cee plus plus) is a general purpose programming language. It has <b>imperative</b>, <b>object-oriented</b> and <b>generic programming</b> features, while also providing the facilities for low level memory manipulation.<br/>It is designed with a bias for <tt>systems programming</tt> (e.g. embedded systems, operating system kernels), with <tt>performance</tt>, <tt>efficiency</tt> and <tt>flexibility</tt> of use as its design requirements. C++ has also been found useful in many other contexts, including desktop applications, servers (e.g. e-commerce, web search, SQL), performance critical applications (e.g. telephone switches, space probes) and entertainment software, such as video games.",
+   "version": "14"
 };
 
 Config.classes = Classes;
