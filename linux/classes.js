a8f39a5684a03915062bf95a54e6a5ac2a28ddd7
Brother Rain <brother.rain.1024@gmail.com>
2014-07-31 04:37:11 +0000

    add function software ssh

--- linux/classes.js
+++ linux/classes.js
@@ -6,4 +6,17 @@ Classes = [
          {"name":"unzip","code":"unzip test.zip -d test","signature":"unzip file.zip -d destination_folder","description":"list, test and extract compressed files in a ZIP archive"},
       ]
    },
+   {
+      "name" : "Processes (todo)",
+      "functions": [  
+         
+      ]
+   },
+   {
+      "name" : "Software",
+      "functions": [  
+         {"name":"ssh-server","code":"sudo apt-get install openssh-server","signature":"","description":"install ssh server "},
+         {"name":"Remote Desktop","code":"sudo apt-get install xrdp\n=> install xrdp for linux","signature":"","description":"Remote desktop server for linux. Using <tt>xrdp</tt> or <tt><a href=\"https://www.nomachine.com/download\">nomachine</a></tt>."},
+      ]
+   },
 ];
