ffb1b7debc4501673de38bc29c38d4b1f60748fb
Brother Rain <brother.rain.1024@gmail.com>
2014-08-01 01:58:03 +0000

    add ml

--- python/config.js
+++ python/config.js
@@ -1,7 +1,10 @@
 Config = {
    "name" : "Python",
    "introduction": "Python is a widely used <b>general-purpose</b>, <b>high-level</b> programming language. Its design <tt>philosophy</tt> emphasizes <b>code readability</b>, and its <tt>syntax</tt> allows <tt>programmers</tt> to <b>express concepts</b> in fewer lines of code than would be possible in languages such as C. The language provides constructs intended to enable clear programs on both a small and large scale.",
-   "version": "2.7"
+   "version": "2.7",
+   "resources": [
+      {"author":"Compileonline.com","title":"Execute Python Script Online.","date":"30 Jul. 2014", "year": "2014", "url":"http://www.compileonline.com/execute_python_online.php"},
+   ]
 };
 
 Config.classes = Classes;
--- template/config.js
+++ template/config.js
@@ -1,7 +1,10 @@
 Config = {
    "name" : "<Module Name>",
    "introduction": "<module introduction>",
-   "version": "<module version>"
+   "version": "<module version>",
+   "resources": [
+      {"author":"<author>","title":"<title>","date":"<date>", "year": "<year>", "url":"<resource_url>"},
+   ]
 };
 
 Config.classes = Classes;
