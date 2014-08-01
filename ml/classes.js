9fb8f7a2c8f6099fa8ccc38e2797ad6eccdb4a10
Brother Rain <brother.rain.1024@gmail.com>
2014-08-01 22:38:30 +0000

    add linux

---
+++ ml/classes.js
@@ -0,0 +1,8 @@
+Classes = [
+   {
+      "name" : "Neophyte",
+      "functions": [
+         {"author":"Colorado Reed","title":"Level-Up Your Machine Learning","time":"Fri Aug 01 2014 08:49:33 GMT+0700 (SE Asia Standard Time)","url":"http://metacademy.org/roadmaps/cjrd/level-up-your-ml","date":"1st Aug. 2014","year":"2014"},
+      ]
+   },
+];
