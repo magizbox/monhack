dcf2c8e1d4d4d5e2ac90da42b1660d402476ea51
rain1024 <rain1024@github.com>
2014-08-03 22:03:45 +0700

    update vim

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,4 @@
 	new WOW().init();
 
 })(jQuery);
+
--- vim/classes.js
+++ vim/classes.js
@@ -1,8 +1,53 @@
 Classes = [
-   {
-      "name" : "File",
-      "functions": [
-         {"name":"mv","code":"mv file1 file2\nmv -rf folder1 folder2","signature":"mv path1 path2","description":"Move (rename) files"},
-      ]
-   },
+    {
+        "name" : "Motion",
+        "functions": [
+            {"name":"h","code":"h\n=> previous character\n2h\n=> 2 previous characters","signature":"h","description":"previous characters\n<img src=\"./images/h.gif\"></img>"},
+            {"name":"j","code":"j\n=> previous character\n2h\n=> 2 previous characters","signature":"j","description":"previous characters\n<img src=\"./images/j.gif\"></img>"},
+            {"name":"k","code":"k\n=> previous character\n2h\n=> 2 previous characters","signature":"k","description":"previous characters\n<img src=\"./images/k.gif\"></img>"},
+            {"name":"l","code":"l\n=> previous character\n2h\n=> 2 previous characters","signature":"l","description":"previous characters\n<img src=\"./images/l.gif\"></img>"},
+        ]
+    },
+    {
+        "name": "Undo/Redo",
+        "functions": [
+            {},
+        ]
+    },
+    {
+        "name": "Search/Replace",
+        "functions": [
+            {},
+        ]
+    },
+    {
+        "name": "Delete",
+        "functions": [
+            {},
+        ]
+    },
+    {
+        "name": "Folding",
+        "functions": [
+            {},
+        ]
+    },
+    {
+        "name": "Windows/Tabs",
+        "functions": [
+            {},
+        ]
+    },
+    {
+        "name": "Autocomplete",
+        "functions": [
+            {},
+        ]
+    },
+    {
+        "name": "NExt",
+        "functions": [
+            {},
+        ]
+    },
 ];
