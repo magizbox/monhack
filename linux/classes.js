2c4ed5912433cf8e746f3d96bb41fc597667a2ef
rain1024 <rain1024@github.com>
2014-08-16 05:52:35 +0700

    update ml

--- linux/classes.js
+++ linux/classes.js
@@ -9,48 +9,48 @@ Classes = [
    {
       "name" : "File Explorer",
       "functions": [
-         {"name":"Dolphin","code":"","signature":"","description":""},
+         {"name":"Dolphin","code":"","signature":"","description":"Dolphin is a free and open source file manager included in the KDE Software Compilation. It is the default file manager in the current version, KDE Software Compilation 4,[3] and can be optionally installed on K Desktop Environment 3. Although replaced as the default file manager for KDE SC 4, Konqueror is still the default web browser, and can be used as an alternative file manager for power users."},
       ]
    },
    {
       "name" : "Process (todo)",
-      "functions": [  
-         
+      "functions": [
+
       ]
    },
    {
       "name" : "System Configuration (todo)",
-      "functions": [  
-         
+      "functions": [
+
       ]
    },
    {
       "name" : "User Enviroments (todo)",
-      "functions": [  
-         
+      "functions": [
+
       ]
    },
    {
       "name" : "Security (todo)",
-      "functions": [  
-         
+      "functions": [
+
       ]
    },
    {
       "name" : "Log (todo)",
-      "functions": [  
-         
+      "functions": [
+
       ]
    },
    {
       "name" : "Bash (todo)",
-      "functions": [  
-         
+      "functions": [
+
       ]
    },
    {
       "name" : "Software",
-      "functions": [  
+      "functions": [
          {"name":"ssh-server","code":"sudo apt-get install openssh-server","signature":"","description":"install ssh server "},
          {"name":"remote desktop","code":"sudo apt-get install xrdp\n=> install xrdp for linux","signature":"","description":"Remote desktop server for linux. Using <tt>xrdp</tt> or <tt><a href=\"https://www.nomachine.com/download\">nomachine</a></tt>."},
       ]
--- ml/classes.js
+++ ml/classes.js
@@ -5,4 +5,10 @@ Classes = [
          {"author":"Colorado Reed","title":"Level-Up Your Machine Learning","time":"Fri Aug 01 2014 08:49:33 GMT+0700 (SE Asia Standard Time)","url":"http://metacademy.org/roadmaps/cjrd/level-up-your-ml","date":"1st Aug. 2014","year":"2014"},
       ]
    },
+   {
+      "name" : "Preprocessing",
+      "functions": [
+          {"name" : ""}
+      ]
+   },
 ];
